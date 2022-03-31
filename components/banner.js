35abb530644f5792a81918a2a4f0774a372021ad
Jose-Antonio Rubio <jadrubio@gmail.com>
2022-03-31 16:40:06 -0400

    added in image for home page and updated stylings

--- components/banner.js
+++ components/banner.js
@@ -8,7 +8,10 @@ const Banner = ({buttonText, handleOnClick}) => {
             <span className={styles.title2}>Connoiseur</span>
         </h1>
         <p className={styles.subTitle}>Discover your local coffee shoppes!</p>
+        <div className={styles.buttonWrapper}>
         <button className={styles.button} onClick={handleOnClick}>{buttonText}</button>
+
+        </div>
     </div>
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 import Banner from "../components/banner";
 
 export default function Home() {
-    const handleOnBannerButtonClick = () => {
-        console.log('Clleeeeekkk');
-    }
+  const handleOnBannerButtonClick = () => {
+    console.log("Clleeeeekkk");
+  };
   return (
     <div className={styles.container}>
       <Head>
@@ -14,8 +15,14 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-          <Banner buttonText={"View stores nearby"} handleOnClick={handleOnBannerButtonClick} />
+        <Banner
+          buttonText={"View stores nearby"}
+          handleOnClick={handleOnBannerButtonClick}
+        />
+        <div className={styles.coffeeImage}>
+          <Image src="/static/banner.png" width={700} height={300} />
+        </div>
       </main>
     </div>
-  )
+  );
 }
